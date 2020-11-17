cd26b1e127db57c83dc44bf8510f31ae08ad0426
Katelyn Suhr <katy@suhr.com>
2020-11-16 18:16:51 -0800

    Adding dynamic css

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -5,38 +5,43 @@ import Person from './Person/Person.js'
 class App extends Component {
   state = {
     persons: [
-      {name: 'Max', age: 28 },
-      {name: 'Manu', age: 29},
-      {name: 'Stephanie', age: 26}
+      {id: '1', name: 'Max', age: 28 },
+      {id: '2', name: 'Manu', age: 29},
+      {id: '3', name: 'Stephanie', age: 26}
     ],
     otherState: 'some other value',
     showPersons: false
 
   }
 
-
-  // method
-  switchNameHandler = (newName) => {
-    // DON'T DO THIS: this.state.persons[0].name = 'Katy';
-
-    // this will override persons but not change other state
-    this.setState({
-      persons:[
-      {name: newName, age: 27 },
-      {name: 'Tim', age: 24},
-      {name: 'Stephanie', age: 26}
-      ]
+  nameChangedHandler = (event, id) => {
+    // find person index
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
     });
-  }
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons:[
-      {name: 'Koji', age: 27 },
-      {name: event.target.value, age: 24},
-      {name: 'Stephanie', age: 26}
-      ]
-    });
+    // get actual person
+
+    // distribute all properties of object we fetch into new
+    // object we are creating
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    // alternative copy object
+    //const person = Object .assign({}, this.state.persons[personIndex]);
+
+    // don't want to mutate reference types directly
+
+    // update person name
+    person.name = event.target.value;
+
+    // update array at position we fetched
+    const persons = [...this.state.persons];
+
+    //update person
+    persons[personIndex] = person;
+    this.setState({persons: persons});
   }
 
   togglePersonsHandler = () => {
@@ -44,39 +49,72 @@ class App extends Component {
     const doesShow = this.state.showPersons;
     // set state merges to the author state
     this.setState({showPersons: !doesShow});
-
-
   }
 
+
+  deletePersonHandler = (personIndex) => {
+    // 1) fetch all the persons
+    // slice copies array
+    // const persons = this.state.persons.slice();
+    // use spread operator using objects from old array
+    const persons = [...this.state.persons];
+
+    // removes 1 element from array
+    persons.splice(personIndex, 1);
+    // set persons to persons constant which
+    // was updated
+    this.setState({persons: persons});
+
+  }
+  // everything in render is always called
+  //this will take individual elements of array
+  // and render them to the DOM if they are valid jsx
   render() {
     const style = {
-        backgroundColor: 'white',
+        backgroundColor: 'green',
+        color: 'white',
         font:'inherit',
         border:'1px solid blue',
         paddding:'8px',
         cursor: 'pointer'
     };
 
+    let persons = null;
+
+    if (this.state.showPersons) {
+      // set persons to jsx code
+      persons = (
+        <div>
+          {this.state.persons.map((person, index)=> {
+            return <Person
+              click={() => this.deletePersonHandler(index)}
+              name={person.name}
+              age={person.age}
+              key={person.id }
+              changed={(event) => this.nameChangedHandler(event, person.id)}/>
+          })}
+        </div>
+      );
+
+      style.backgroundColor = 'red';
+    }
+
+    const classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push('red'); // classes = ['red']
+    }
+
+    if(this.state.persons.length <= 1) {
+      classes.push('bold'); // classes = ['red', 'bold']
+    }
     return (
       <div className="App">
       <h1> Hi, I'm a React App</h1>
-      <p> This is working</p>
+      <p className={classes.join(' ')}> This is working</p>
       <button
         style={style}
-        onClick={this.togglePersonsHandler}>Switch Name</button>
-      { this.state.showPersons === true ?
-        <div>
-          <Person
-            name={this.state.persons[0].name}
-            age={this.state.persons[0].age} />
-          <Person
-            name={this.state.persons[1].name}  age={this.state.persons[1].age}
-            click={this.switchNameHandler.bind(this, 'Timothy Antonio')}
-            changed={this.nameChangedHandler}> My hobbies: singing</Person>
-          <Person
-            name={this.state.persons[2].name}  age={this.state.persons[2].age} />
-        </div> : null
-      }
+        onClick={this.togglePersonsHandler}>Toggle Persons</button>
+      {persons}
     </div>
     );
     // return React.createElement('div', {className:'App'}, React.createElement('h1', null, 'Does this work now?'));
