c9fcb6fe157aef8bac28586ebce818f03ad0b8a5
Katelyn Suhr <katy@suhr.com>
2020-11-23 15:49:29 -0800

    Set up code into different components

--- react-complete-guide/src/containers/App.js
+++ react-complete-guide/src/containers/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import classes from  './App.css';
-import Person from '../components/Persons/Persons';
+import Persons from '../components/Persons/Persons';
 import ErrorBoundary from '../components/ErrorBoundary/ErrorBoundary';
+import Cockpit from '../components/Cockpit/Cockpit';
+
+//this container manages state and maniputlates state
 
 class App extends Component {
   state = {
@@ -72,37 +75,25 @@ class App extends Component {
   // and render them to the DOM if they are valid jsx
   render() {
     let persons = null;
-    let btnClass = '';
-
 
     if (this.state.showPersons) {
       // set persons to jsx code
       persons = (
-        <div>
-          <Person
+          <Persons
           persons={this.state.persons}
           clicked={this.deletePersonHandler}
           changed={this.nameChangedHandler} />
-        </div>
       );
-      btnClass = classes.Red;
     }
 
-    const assignedClasses = [];
-    if (this.state.persons.length <= 2) {
-      assignedClasses.push(classes.red); // classes = ['red']
-    }
 
-    if(this.state.persons.length <= 1) {
-      assignedClasses.push(classes.bold); // classes = ['red', 'bold']
-    }
     return (
         <div className={classes.App}>
-        <h1> Hi, I'm a React App</h1>
-        <p className={assignedClasses.join(' ')}> This is working</p>
-        <button className={btnClass}
-          onClick={this.togglePersonsHandler}>Toggle Persons
-        </button>
+          <Cockpit
+            title={this.props.appTitle}
+            showPersons={this.state.showPersons}
+            persons={this.state.persons}
+            clicked={this.togglePersonsHandler}/>
         {persons}
         </div>
     );
